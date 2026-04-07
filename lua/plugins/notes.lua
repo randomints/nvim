588065140d56d222a70eff8900b450fcdfe02135
Raahim Shahzad Azfar <randomints@Raahims-MacBook-Air.local>
2026-04-07 22:50:12 +0300

    just in case i didnt back it up

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,19 +1,48 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/Documents/notes/**/*",
+        org_default_notes_file = "~/Documents/notesrefile.org",
+      })
+
+      -- Experimental LSP support
+      vim.lsp.enable("org")
+    end,
+  },
+
+  {
+    "akinsho/org-bullets.nvim",
+  },
+
+  {
+    "Thiago4532/mdmath.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+
+  {
+    "obsidian-nvim/obsidian.nvim",
+    ft = "markdown",
     opts = {
-      checkbox = {
-        checked = { icon = "◉ " },
-        unchecked = { icon = "○ " },
-        custom = { todo = { rendered = "⭐︎ " } },
+      legacy_commands = false,
+      workspaces = {
+        { name = "personal", path = "~/Documents/notes" },
       },
     },
   },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+    opts = {},
+  },
+
   {
     "3rd/image.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -36,6 +65,7 @@ return {
       })
     end,
   },
+
   {
     "lervag/vimtex",
     lazy = false,
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,26 +1,10 @@
 return {
-  -- nvim-cmp keybindings
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping = {
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-      }
-    end,
-  },
-
   -- flash.nvim keybindings
   {
     "folke/flash.nvim",
     keys = {
       {
-        "zk",
+        "ak",
         mode = { "n", "x", "o" },
         function()
           require("flash").jump()
@@ -28,7 +12,7 @@ return {
         desc = "Flash",
       },
       {
-        "Zk",
+        "aK",
         mode = { "n", "x", "o" },
         function()
           require("flash").treesitter()
@@ -60,79 +44,6 @@ return {
         desc = "Toggle Flash Search",
       },
     },
-  },
-
-  -- none-ls keybindings
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-          require("none-ls.diagnostics.eslint_d"),
-          require("none-ls.code_actions.eslint_d"),
-        },
-      })
-
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
-
-  -- obsidian + markdown + image + vimtex
-  {
-    "obsidian-nvim/obsidian.nvim",
-    ft = "markdown",
-    opts = {
-      legacy_commands = false,
-      workspaces = {
-        { name = "personal", path = "~/Documents/notes" },
-      },
-    },
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
-    opts = {},
-  },
-  {
-    "3rd/image.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("image").setup({
-        backend = "kitty",
-        window_overlap_clear_enabled = true,
-        editor_only_render_when_focused = true,
-        integrations = {
-          markdown = {
-            enabled = true,
-            only_render_image_at_cursor = false,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-          },
-        },
-        max_width = 80,
-        max_height = 20,
-        max_width_window_percentage = 50,
-      })
-    end,
-  },
-  {
-    "lervag/vimtex",
-    lazy = false,
-    config = function()
-      vim.g.vimtex_view_method = "skim"
-      vim.g.vimtex_view_forward_search_on_start = false
-      vim.g.vimtex_compiler_latexmk = {
-        aux_dir = "/Users/raahimshahzadazfar/.texfiles/",
-        out_dir = "/Users/raahimshahzadazfar/.texfiles/",
-      }
-    end,
   },
 
   -- snacks.nvim keybindings
